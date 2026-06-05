7348eecf2aba42184ccd4e9fa12ed44c52c7e34b
Lyof <florian.lyof@laposte.net>
2026-06-05 20:15:59 +0200

    javadocs moment

--- common/src/main/java/net/lcc/sollib/api/common/data/runtime/LoadCondition.java
+++ common/src/main/java/net/lcc/sollib/api/common/data/runtime/LoadCondition.java
@@ -4,12 +4,53 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.lcc.sollib.SolTest;
+import net.lcc.sollib.api.common.config.ConfigEntry;
 import net.lcc.sollib.api.common.SolRegistries;
 import net.lcc.sollib.platform.Services;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.util.GsonHelper;
 
+/**
+ * <b>Any</b> datapack file can be given load conditions, and will only actually be read if they are met. <br>
+ * To do so, simply add a <i>condition</i> field to the file, like so:
+ * <pre>
+ * {
+ *   // Here be the rest of your json file
+ *
+ *   "condition": {
+ *       "type": conditiontype
+ *   }
+ * }
+ * </pre>
+ *
+ * There currently are 4 types of conditions: <br/>
+ * - "dependency": Takes an extra <i>mod</i> argument. File will only be loaded if said namespace is running. <br/>
+ * - "config": Takes an extra <i>entry</i> argument. File will only be loaded if the corresponding {@link ConfigEntry} evaluates to true.
+ * - "and": Takes a list of conditions in a <i>conditions</i> field. Yields true only if every sub condition is true. <br/>
+ * - "or": Takes a list of conditions in a <i>conditions</i> field. Yields true if at least one sub condition is true.
+ * <br/> <br/>
+ *
+ * For example:
+ * <pre>
+ * {
+ *   "message": "Hello World!",
+ *   "condition": {
+ *     "type": "and",
+ *     "conditions": [
+ *       {
+ *         "type": "config",
+ *         "entry": "sollib/test:test_category.nested.exists"
+ *       },
+ *       {
+ *         "type": "dependency",
+ *         "mod": "fabric"
+ *       }
+ *     ]
+ *   }
+ * }
+ * </pre>
+ */
 public class LoadCondition {
     public static Resource apply(ResourceLocation id, Resource resource) {
         if (id.getPath().endsWith(".json")) {
