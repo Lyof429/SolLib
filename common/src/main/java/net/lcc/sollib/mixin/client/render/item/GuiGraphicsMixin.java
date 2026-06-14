2f1a0145453118ea60c74676558b0d85d19f6c1e
Lyof <florian.lyof@laposte.net>
2026-06-14 22:26:12 +0200

    fixed added bars always rendering

--- common/src/main/java/net/lcc/sollib/mixin/client/render/item/GuiGraphicsMixin.java
+++ common/src/main/java/net/lcc/sollib/mixin/client/render/item/GuiGraphicsMixin.java
@@ -18,7 +18,7 @@ public abstract class GuiGraphicsMixin {
     @Inject(method = "renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
             at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;popPose()V", shift = At.Shift.BEFORE))
     public void renderAddedBar(Font textRenderer, ItemStack stack, int x, int y, String countOverride, CallbackInfo ci) {
-        if (stack.getItem() instanceof IAddedBarItem barAdder) {
+        if (stack.getItem() instanceof IAddedBarItem barAdder && barAdder.shouldAddBarRender(stack)) {
             int i = Math.round(barAdder.getAddedBarFullness(stack) * 13);
             int j = barAdder.getAddedBarColor(stack);
             this.fill(RenderType.guiOverlay(), x + 2, y + 14, x + 15, y + 15, -16777216);
