a48486fd25abfbac64ac7fab4da70684daf60971
Lyof <florian.lyof@laposte.net>
2026-06-16 19:46:39 +0200

    stopped tests from firing, as they were messing up dependents

--- common/src/main/java/net/lcc/sollib/SolLib.java
+++ common/src/main/java/net/lcc/sollib/SolLib.java
@@ -3,21 +3,12 @@ package net.lcc.sollib;
 import net.lcc.sollib.api.common.registry.SolModContainer;
 import net.lcc.sollib.api.common.registry.holder.DensityFunctionHolder;
 import net.lcc.sollib.api.common.worldgen.density.ProgressionDensityFunction;
-import net.lcc.sollib.platform.Services;
 
 public class SolLib {
     public static final String MOD_ID = "sollib";
     public static final SolModContainer MOD = new SolModContainer("SolLib", MOD_ID);
 
     public static void init() {
-        if (Services.PLATFORM.isDevelopmentEnvironment()) {
-            MOD.getLogger().info("Running tests for Lyof");
-            SolTest.lyof();
-
-            MOD.getLogger().info("Running tests for Sasha");
-            SolTest.sasha();
-        }
-
         MOD.register(DensityFunctionHolder.class, "progression", () -> ProgressionDensityFunction.CODEC);
     }
 }
