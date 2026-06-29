ed16f82b4be6464cedece2eefa5171637a60d717
SashaKYotoz <63205134+SashaKYotoz@users.noreply.github.com>
2026-06-29 16:48:42 +0300

    correct gui methods for 1.21.1

--- common/src/main/java/net/lcc/sollib/api/client/ui/bossbar/SBossBarRegistry.java
+++ common/src/main/java/net/lcc/sollib/api/client/ui/bossbar/SBossBarRegistry.java
@@ -1,6 +1,7 @@
 package net.lcc.sollib.api.client.ui.bossbar;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.BossEvent;
 
 import java.util.LinkedHashMap;
@@ -9,41 +10,51 @@ import java.util.function.Predicate;
 
 public class SBossBarRegistry {
     public static final SBossBarRegistry INSTANCE = new SBossBarRegistry();
-    private SBossBarRegistry() {}
+
+    private SBossBarRegistry() {
+    }
 
     private final Map<Predicate<BossEvent>, IBossBarRenderer> INSTANCES = new LinkedHashMap<>();
 
     /**
      * Manages registration of custom boss bar render introduced in SolLib
+     *
      * @param condition Filters the actual boss bar event to be processed by {@link IBossBarRenderer}
-     * @param renderer Management of how custom boss bar should be rendered
-     * @since 1.0.0
+     * @param renderer  Management of how custom boss bar should be rendered
+     * @since 1.0
      */
     public void register(Predicate<BossEvent> condition, IBossBarRenderer renderer) {
         INSTANCES.put(condition, renderer);
     }
 
     /**
+     * Redeclaration of vanilla BAR_BACKGROUND_SPRITES and BAR_PROGRESS_SPRITES from BossHealthOverlay
+     */
+    private static final ResourceLocation[] BAR_BACKGROUND_SPRITES = new ResourceLocation[]{ResourceLocation.withDefaultNamespace("boss_bar/pink_background"), ResourceLocation.withDefaultNamespace("boss_bar/blue_background"), ResourceLocation.withDefaultNamespace("boss_bar/red_background"), ResourceLocation.withDefaultNamespace("boss_bar/green_background"), ResourceLocation.withDefaultNamespace("boss_bar/yellow_background"), ResourceLocation.withDefaultNamespace("boss_bar/purple_background"), ResourceLocation.withDefaultNamespace("boss_bar/white_background")};
+    private static final ResourceLocation[] BAR_PROGRESS_SPRITES = new ResourceLocation[]{ResourceLocation.withDefaultNamespace("boss_bar/pink_progress"), ResourceLocation.withDefaultNamespace("boss_bar/blue_progress"), ResourceLocation.withDefaultNamespace("boss_bar/red_progress"), ResourceLocation.withDefaultNamespace("boss_bar/green_progress"), ResourceLocation.withDefaultNamespace("boss_bar/yellow_progress"), ResourceLocation.withDefaultNamespace("boss_bar/purple_progress"), ResourceLocation.withDefaultNamespace("boss_bar/white_progress")};
+
+    /**
      * Variant of {@link #register(Predicate, IBossBarRenderer)} of boss bar render registration with default placement
      * Processes only custom texture to render for boss bar
-     * @since 1.0.0
+     *
+     * @since 1.0
      */
     public void register(Predicate<BossEvent> condition, ResourceLocation texture) {
         INSTANCES.put(condition, ((guiGraphics, x, y, bossEvent) -> {
-            guiGraphics.blit(texture, x, y, 0, bossEvent.getColor().ordinal() * 5 * 2 + 5, (int)(bossEvent.getProgress() * 183.0F), 5);
-            guiGraphics.blit(texture, x, y - 2, 0, 0, 183, 9, 183, 9);
-
-            int progressWidth = (int) (bossEvent.getProgress() * 183.0F);
-            if (progressWidth > 0) {
-                guiGraphics.blit(texture, x, y - 2, 0, 9, progressWidth, 9, 183, 9);
-            }
+            int progress = Mth.lerpDiscrete(bossEvent.getProgress(), 0, 182);
+            guiGraphics.blitSprite(BAR_BACKGROUND_SPRITES[bossEvent.getColor().ordinal()], 182, 5, 0, 0, x, y, 182, 5);
+            guiGraphics.blitSprite(texture, 183, 9, 0, 0, x, y - 2, 183, 9);
+            if (progress > 0)
+                guiGraphics.blitSprite(BAR_PROGRESS_SPRITES[bossEvent.getColor().ordinal()], 182, 5, 0, 0, x, y, progress, 5);
         }));
     }
 
     /**
-    * Processes custom renderers registered by {@link #register(Predicate, IBossBarRenderer)} and used by {@link net.lcc.sollib.mixin.client.BossHealthOverlayMixin}
-    * @since 1.0.0
-    * */
+     * Processes custom renderers registered by {@link #register(Predicate, IBossBarRenderer)} and used by {@link net.lcc.sollib.mixin.client.BossHealthOverlayMixin}
+     *
+     * @since 1.0
+     *
+     */
     public IBossBarRenderer getRenderer(BossEvent bossEvent) {
         for (Map.Entry<Predicate<BossEvent>, IBossBarRenderer> entry : INSTANCES.entrySet()) {
             if (entry.getKey().test(bossEvent)) {
