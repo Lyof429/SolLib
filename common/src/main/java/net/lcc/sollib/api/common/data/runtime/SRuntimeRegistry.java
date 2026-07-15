815aec27f1ed544af88645591b158ed429f22bc0
Lyof <florian.lyof@laposte.net>
2026-07-15 20:51:58 +0200

    forgot to version bump

--- common/src/main/java/net/lcc/sollib/api/common/data/runtime/SRuntimeRegistry.java
+++ common/src/main/java/net/lcc/sollib/api/common/data/runtime/SRuntimeRegistry.java
@@ -3,7 +3,6 @@ package net.lcc.sollib.api.common.data.runtime;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
-import net.lcc.sollib.api.common.data.runtime.condition.LoadCondition;
 import net.lcc.sollib.api.common.logger.SolLogger;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.Resource;
