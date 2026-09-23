bf9ce222bf19e0d95e19b88b6b46724ec4e0bf93
Lyof <florian.lyof@laposte.net>
2026-09-23 18:28:29 +0200

    damnit i'm stupid

--- common/src/main/java/net/lcc/sollib/SolLib.java
+++ common/src/main/java/net/lcc/sollib/SolLib.java
@@ -2,12 +2,10 @@ package net.lcc.sollib;
 
 import net.lcc.sollib.api.common.SolRegistries;
 import net.lcc.sollib.api.common.config.SolConfig;
-import net.lcc.sollib.api.common.data.reload.IReloadListener;
 import net.lcc.sollib.api.common.registry.SolModContainer;
 import net.lcc.sollib.api.common.registry.holder.DensityFunctionHolder;
 import net.lcc.sollib.api.common.worldgen.density.ProgressionDensityFunction;
 import net.lcc.sollib.api.event.SEvents;
-import net.minecraft.server.packs.resources.ResourceManager;
 
 public class SolLib {
     public static final String MOD_ID = "sollib";
@@ -21,21 +19,6 @@ public class SolLib {
     public static void init() {
         SEvents.load();
         MOD.createConfig(MOD_ID, 1.0, builder -> {});
-
-        SolRegistries.Data.RELOAD.register(new IReloadListener() {
-            @Override
-            public void preload(ResourceManager manager) {
-                MOD.getLogger().info("PRELOAD");
-                manager.listResources("potion", name -> true).forEach((id, resource) -> {
-                    MOD.getLogger().info(id);
-                });
-            }
-
-            @Override
-            public void reload(ResourceManager manager) {
-                MOD.getLogger().info("RELOAD");
-            }
-        });
 
         MOD.register(DensityFunctionHolder.class, "progression", () -> ProgressionDensityFunction.CODEC);
     }
